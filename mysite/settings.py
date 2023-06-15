5cb49e8aea80d2fe16bbe7829b57639d7eac4d45
jhanceboyz <40009726+jhanceboyz@users.noreply.github.com>
2023-06-14 21:13:45 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)so5l1gnu%c3f8er_(dx6%phuvua%x(b1-xt)v177t#^_-r@x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
